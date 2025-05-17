ccf3669044f46b5bb0328a9aba72d1e71ad176f4
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-05-17 22:53:42 +0500

    Working with Model

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/product.model.js')
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.post('/api/products', async(req, res)=>{
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+    
+})
 
 mongoose.connect("mongodb+srv://azmeerhassanammad:iIF1XbagRo6eFtYb@practice-api-express-mo.8asxe2c.mongodb.net/practice-API-Express-MongoDB?retryWrites=true&w=majority&appName=practice-API-Express-MongoDB")
 .then(()=>{
